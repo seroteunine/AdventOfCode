18f4c5f254dff8a96dfb0a986ffccdf1f7a474dc
seroteunine <teun.vandalen.tvd@gmail.com>
2023-12-13 12:12:52 +0100

    find the row where there is only one mistake

--- 2023/13.py
+++ 2023/13.py
@@ -5,16 +5,33 @@ def findLine(G):
             steps = min(r + 1, R - r - 1)
             if all(G[r - d] == G[r + 1 + d] for d in range(1, steps)):
                 return r + 1
-    return None
+    return 0
 
 t = 0
 for block in open('13.txt').read().split('\n\n'):
     G = block.split('\n')
-    l = findLine(G)
-    if l:
-        t += l * 100
-        continue
-    t += findLine(list(zip(*G)))
+    r, c = findLine(G), findLine(list(zip(*G)))
+    t += r * 100 + c
 
-print(t)    
+print(t)      
 
+def findLineImperfect(G):
+    R = len(G)
+    for r in range(R - 1):
+        mistakes = 0
+        steps = min(r + 1, R - r - 1)
+        for i in range(0, steps):
+            for c in range(len(G[r])):
+                if G[r - i][c] != G[r + 1 + i][c]:
+                    mistakes += 1
+        if mistakes == 1:
+            return r + 1
+    return 0
+
+t = 0
+for block in open('13.txt').read().split('\n\n'):
+    G = block.split('\n')
+    r, c = findLineImperfect(G), findLineImperfect(list(zip(*G)))
+    t += r * 100 + c
+
+print(t)      
